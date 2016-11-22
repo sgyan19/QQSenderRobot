cfe213da2ce158ce2aac701bc2c401912a584fdd
孙国耀 <guoyao@staff.sina.com.cn>
2016-11-22 17:41:19 +0800

    token 获取器

---
+++ WeiboTokenAuthor2.0/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeiboTokenAuthor2._0
+{
+    public partial class Form1 : Form
+    {
+        #region 微博列表的模板
+        const string htmlPattern = @"<!DOCTYPE html>
+<html lang=""en"" xmlns=""http://www.w3.org/1999/xhtml"">
+<head>
+	<title></title>
+	<style type=""text/css"">
+		html, body {
+			font-size: 12px;
+			cursor: default;
+			padding: 5px;
+			margin: 0;
+			font-family:微软雅黑,Tahoma;
+		}
+
+		div.status {
+			padding-left: 60px;
+			position: relative;
+			margin-bottom: 10px;
+			min-height:80px;
+			_height:80px;
+		}
+
+			div.status p {
+				margin: 0 0 5px 0;
+				line-height: 1.5;
+				padding: 0;
+			}
+
+				div.status p span.name {
+					color: #369;
+				}
+
+				div.status p.status-content {
+					color: #333;
+				}
+
+				div.status p.status-count {
+					color:#999;
+				}
+
+			div.status .face {
+				position: absolute;
+				left: 0;
+				top: 0;
+			}
+
+			div.status div.repost {
+				border: solid 1px #ACD;
+				background: #F0FAFF;
+				padding: 10px 10px 0 10px;
+			}
+
+		div.repost p.repost-content {
+			color: #666 !important;
+		}
+	</style>
+</head>
+<body>
+<!--StatusesList-->
+</body>
+</html>";
+        const string imageParttern = @"<img src=""{0}"" alt=""图片"" class=""inner-pic"" />";
+        const string statusPattern = @"	<div class=""status"">
+		<img src=""{0}"" alt=""{1}"" class=""face"" />
+		<p class=""status-content""><span class=""name"">{1}</span>：{2}</p>
+		{3}
+		<p class=""status-count"">转发({4}) 评论({5})</p>
+	</div>
+";
+        const string repostPattern = @"	<div class=""status"">
+		<img src=""{0}"" alt=""{1}"" class=""face"" />
+		<p class=""status-content""><span class=""name"">{1}</span>：{2}</p>
+		<div class=""repost"">
+			<p class=""repost-cotent""><span class=""name"">@{3}</span>：{4}</p>
+			{5}
+			<p class=""status-count"">转发({6}) 评论({7})</p>
+		</div>
+		<p class=""status-count"">转发({8}) 评论({9})</p>
+	</div>
+";
+        #endregion
+
+        private NetDimension.OpenAuth.Sina.SinaWeiboClient openAuth;
+        public Form1(NetDimension.OpenAuth.Sina.SinaWeiboClient client)
+        {
+            InitializeComponent();
+            this.openAuth = client;
+            textBox1.Text = client.AccessToken;
+        }
+
+    }
+}
---
+++ WeiboTokenAuthor2.0/Program.cs
@@ -0,0 +1,39 @@
+﻿using NetDimension.OpenAuth.Sina;
+using NetDimension.OpenAuth.Winform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeiboTokenAuthor2._0
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run();
+
+            //请自行修改appKey，appSecret和回调地址。winform的回调地址可以是一个随便可以访问的地址，貌似不可以访问的地址也是可以的，只要URL中带着Code就行
+            //var client = new SinaWeiboClient("1402038860", "62e1ddd4f6bc33077c796d5129047ca2", "http://qcyn.sina.com.cn");
+            var client = new SinaWeiboClient("1445565505", "57b12ef7d52eddd725f76c278fcbab47", "http://hanclt.eicp.net:8088");
+
+            //NetDimension.OpenAuth.Winform封装的一个登录窗口，主要远离就是个WebBrowser控件，然后在该控件的导航事件里面监测Url是不是带有Code，如果有就调用GetAccessTokenByCode
+            var authForm = client.GetAuthenticationForm();
+
+            authForm.StartPosition = FormStartPosition.CenterScreen;
+            //authForm.Icon = Properties.Resources.icon_form;
+
+            if (authForm.ShowDialog() == DialogResult.OK)
+            {
+                Application.Run(new Form1(client));
+            }
+        }
+    }
+}
